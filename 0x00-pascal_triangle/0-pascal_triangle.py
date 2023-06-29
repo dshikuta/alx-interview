4db561323c77f43a362e0eabbd8d12f6d3ce65e2
dshikuta <dshikuta@gmail.com>
2023-06-29 20:55:48 +0000

    a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Pascal Trinagle a triangular arrangement of numbers where each number in the triangle is the sum of the two numbers directly above it.
+"""
+
+def pascal_triangle(n):
+"""returns the pascal triangle
+"""
+
+  pascal = []
+
+    if n <= 0:
+        return []
+
+
+    for i in range(n):
+
+       list = [1] * (i + 1)
+        if i >= 2:
+            for j in range(1, i):
+                list[j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
+        pascal.append(list)
+"""add two previous numbers
+"""
+
+    return pascal
